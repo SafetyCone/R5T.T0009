ad66615220cac7a91496766aafa96b477cd9c6ea
David Coats <david@getsafetycone.com>
2020-08-10 00:43:48 -0700

    Added the UserName type.

---
+++ source/R5T.T0009/Code/Classes/UserName.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using R5T.Stagira;
+
+
+namespace R5T.T0009
+{
+    public class UserName : TypedString
+    {
+        #region Static
+
+        public static UserName From(string userNameValue)
+        {
+            var userName = new UserName(userNameValue);
+            return userName;
+        }
+
+        #endregion
+
+
+        public UserName(string value)
+            : base(value)
+        {
+        }
+    }
+}
